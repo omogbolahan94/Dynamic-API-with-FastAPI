a042d80f3a3d433c436990f384769f857205275f
omogbolahan94 <gabrielomogbolahan1@gmail.com>
2025-07-31 18:51:37 +0100

    set schema for post body

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+database = [{"title": "We are gods","message": "We have the power to create and destroy","id": 1},
+        {"title": "What us love","message": "God is love","id": 2}
+]
 
 
 class Post(BaseModel):
     title: str
     message: str
     id: int
+    rating: Optional[int] = None
 
 
 @app.get("/posts/")
 async def post():
-    return {"message": "Hello World"}
+    return  {"data": database}
 
 
-@app.post("/posts/")
+@app.get("/posts/{id}")
+def get_post(id: int):
+    
+    def find_post():
+        for post in database:
+            if post['id'] == id:
+                return post
+    result = find_post()
+
+    if not result:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id {id} was not found")
+
+    return result
+
+
+@app.post("/posts/", status_code=status.HTTP_201_CREATED)
 async def post(post: Post):
-    return post
+    database.append(post)
+    return {"data": post}
+
