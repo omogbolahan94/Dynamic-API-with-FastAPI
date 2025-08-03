055ad99d2c5f37519da7f40bbdbac878b3b9e3a5
omogbolahan94 <gabrielomogbolahan1@gmail.com>
2025-08-03 06:26:46 +0100

    users model

--- app/models.py
+++ app/models.py
@@ -3,9 +3,8 @@ from sqlalchemy import ForeignKey
 from sqlalchemy import String, Text, DateTime, Boolean, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
 from .database import Base
+from datetime import datetime
 
 
 
@@ -15,7 +14,16 @@ class Post(Base):
     title: Mapped[str] = mapped_column(String(50), nullable=False)
     content: Mapped[str] = mapped_column(String(50), nullable=False)
     published: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
-    created_date: Mapped[bool] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
-    def __repr__(self) -> str:
-        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+    created_date: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    
+    # def __repr__(self) -> str:
+    #     return f"Post(title={self.title!r}, published={self.published!r})"
+
+
+class Users(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(50), nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+
 
