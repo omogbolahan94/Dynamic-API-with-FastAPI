f05ef7d9fe44b0cd6bf011baa20879b4dc779b30
omogbolahan94 <gabrielomogbolahan1@gmail.com>
2025-08-16 23:27:51 +0100

    included cors for all domain

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database import Base, engine
 from .routers import posts, users, auth, votes
 
@@ -7,6 +8,16 @@ from .routers import posts, users, auth, votes
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [*]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(posts.router)
 app.include_router(users.router)
