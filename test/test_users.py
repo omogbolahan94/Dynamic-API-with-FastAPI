b304e09e4cf51d8dcdcca66910a704ebd5e4c712
omogbolahan94 <gabrielomogbolahan1@gmail.com>
2025-09-02 11:25:32 +0100

    tested user creation

--- test/test_users.py
+++ test/test_users.py
@@ -6,8 +6,9 @@ from app.config import settings
 
 def test_create_user(client):
     res = client.post(
-        "/users/", json={"email": "hello123@gmail.com", "password": "password123"})
+        "/users/", json={"email": "hello123@gmail.com", "password": "password123", "id": 1})
 
-    new_user = schemas.UserOut(**res.json())
+    print(res.json())
+    new_user = schemas.UserResponse(**res.json())
     assert new_user.email == "hello123@gmail.com"
     assert res.status_code == 201
